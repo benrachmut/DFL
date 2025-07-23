30487ccc78b60e40b5079e436c0372546f66004b
ben_rachmut <rachmut.ben@gmail.com>
2025-07-23 12:18:54 -0500

    ...

--- ReadResults.py
+++ ReadResults.py
@@ -4,7 +4,7 @@ import pickle
 from config_ import Algorithm, NetType
 
 
-def get_avg_per_client(param):
+def get_avg_per_client(selected_data,param):
     nums = getattr(selected_data, param)
     dict_all_iters = {}
     for client_id,dict_per_iter in nums.items():
@@ -18,20 +18,109 @@ def get_avg_per_client(param):
     return ans
 
 
-if __name__ == '__main__':
-
+def create_data_per_measure(measure_ ):
     with open('CIFAR10,SparseRandom,25,5,1,10,IID,DMAPL,Distributed,VGG,AlexNet.pkl', 'rb') as f:
         data = pickle.load(f)
+    for algo in algos:
+        for strong_net in [NetType.VGG]:
+            selected_data = data[algo][strong_net][0]
+            if "With Hubs" not in for_graph[25]:
+                for_graph[25]["With Hubs"] = {}
+            for_graph[25]["With Hubs"][alt_measure_names[measure_]] = get_avg_per_client(selected_data,measure_)
 
-    print(data)
+    with open('CIFAR10,SparseRandom,25,5,1,10,IID,DMAPL,Distributed,AlexNet,AlexNet.pkl', 'rb') as f:
+        data = pickle.load(f)
+    for algo in algos:
+        for strong_net in [NetType.AlexNet]:
+            selected_data = data[algo][strong_net][0]
+            if "Without Hubs" not in for_graph[25]:
+                for_graph[25]["Without Hubs"] = {}
+
+            for_graph[25]["Without Hubs"][alt_measure_names[measure_]] = get_avg_per_client(selected_data,measure_)
+
+    with open('CIFAR10,SparseRandom,10,5,1,10,IID,DMAPL,Distributed,VGG,AlexNet.pkl', 'rb') as f:
+        data = pickle.load(f)
+    for algo in algos:
+        for strong_net in [NetType.VGG]:
+            selected_data = data[algo][strong_net][0]
+            if "With Hubs" not in for_graph[10]:
+                for_graph[10]["With Hubs"] = {}
+            for_graph[10]["With Hubs"][alt_measure_names[measure_]] = get_avg_per_client(selected_data,measure_)
+
+    with open('CIFAR10,SparseRandom,10,5,1,10,IID,DMAPL,Distributed,AlexNet,AlexNet.pkl', 'rb') as f:
+        data = pickle.load(f)
+    for algo in algos:
+        for strong_net in [NetType.AlexNet]:
+            selected_data = data[algo][strong_net][0]
+            if "Without Hubs" not in for_graph[10]:
+                for_graph[10]["Without Hubs"] = {}
+
+            for_graph[10]["Without Hubs"][alt_measure_names[measure_]] = get_avg_per_client(selected_data,measure_)
+
+import matplotlib.pyplot as plt
+
+def plot_nested_dict(data, x_label="X-axis", y_label="Y-axis", title="Graph"):
+    """
+    Plots a nested dictionary where:
+      - The outer key (e.g., 'with hub', 'without hub') determines the color.
+      - The middle key ('self' or 'Best Neighbor') determines the line style.
+      - The innermost dictionary maps x-values to y-values.
+
+    Args:
+        data (dict): Nested dictionary to plot.
+        x_label (str): Label for the X-axis.
+        y_label (str): Label for the Y-axis.
+        title (str): Title of the plot.
+    """
+
+    # Assign unique colors to outer keys
+    color_palette = plt.get_cmap("tab10")
+    outer_keys = list(data.keys())
+    colors = {key: color_palette(i) for i, key in enumerate(outer_keys)}
+
+    # Define line styles
+    linestyles = {
+        'self': 'solid',
+        'Best Neighbor': 'dashed'
+    }
+
+    plt.figure(figsize=(8, 5))
+
+    for outer_key, inner_dict in data.items():
+        for line_type, xy_vals in inner_dict.items():
+            x = list(xy_vals.keys())
+            y = list(xy_vals.values())
+            plt.plot(
+                x, y,
+                label=f"{outer_key} - {line_type}",
+                color=colors[outer_key],
+                linestyle=linestyles.get(line_type, 'solid')  # Default to solid
+            )
+
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+
 
     algos =  [Algorithm.DMAPL]
     strong_nets = [NetType.VGG]
-    for algo in algos:
-        for strong_net in strong_nets:
-            selected_data = data[algo][strong_net][0]
-            avg_best_n = get_avg_per_client("client_best_neighbor_model_accuracy_1")
-            avg_personal = get_avg_per_client("client_self_model_accuracy_1")
-            #avg_best_n = get_avg_per_client("client_loss_test")
-            #avg_personal = get_avg_per_client("client_loss_train")
-            print()
+
+    for_graph = {}
+    for_graph[25] = {}
+    for_graph[10] = {}
+    alt_measure_names = {"client_best_neighbor_model_accuracy_1":"Best Neighbor","client_self_model_accuracy_1":"Self"}
+
+    create_data_per_measure("client_best_neighbor_model_accuracy_1")
+    create_data_per_measure("client_self_model_accuracy_1")
+
+    plot_nested_dict(for_graph[25],x_label="Iteration", y_label="Top 1 Accuracy", title="25 Clients")
+    plot_nested_dict(for_graph[10],x_label="Iteration", y_label="Top 1 Accuracy", title="10 Clients")
+
+
+    print()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def extract_info(clients,t):
     measure_neighbors(clients, t)
 
     rd = RecordData(clients)
-    data_to_pickle[ec.algorithm][ec.client_hub_net][num_run] = rd
+    data_to_pickle[ec.data_set.name][ec.topology_technique][ec.num_clients][ec.client_hub_net] = rd
     name_pkl = str(ec)+ ".pkl"
     with open(name_pkl, 'wb') as f:
         pickle.dump(data_to_pickle, f)
@@ -15,9 +15,11 @@ def extract_info(clients,t):
 if __name__ == '__main__':
     print("Device:", device)
 
-    ec.data_set = DataSet.CIFAR10
-    ec.topology_technique = TopologyTechnique.SparseRandom
-    ec.num_clients = 10
+    data_sets = [DataSet.CIFAR10]
+    topologies = [TopologyTechnique.DenseRandom]
+    client_sizes = [10]
+    client_hub_nets = [NetType.VGG]
+
     ec.num_of_hubs = 5
     ec.num_runs = 1
     ec.data_distribution = DataDistribution.IID
@@ -26,36 +28,47 @@ if __name__ == '__main__':
     ec.client_non_hub_net = NetType.AlexNet
 
     ec.environment = Env.Distributed
-    client_hub_nets = [NetType.VGG]
-    algorithms = [Algorithm.DMAPL]
+    ec.algorithm = Algorithm.DMAPL
     data_to_pickle = {}
 
-    for algo in algorithms:
-        ec.algorithm = Algorithm.DMAPL
-        data_to_pickle[ec.algorithm] = {}
-        for client_hub_net in client_hub_nets:
-            ec.client_hub_net = client_hub_net
-            data_to_pickle[ec.algorithm][ec.client_hub_net] = {}
 
-            for num_run in range(ec.num_runs):
-                #data_to_pickle[ec.algorithm][ec.client_hub_net][num_run] = {}
+    for data_set in data_sets:
+        ec.data_set = data_set
+        data_to_pickle[ec.data_set.name] = {}
 
-                ec.num_run = num_run
-                ec.neighbors_dict = get_neighbors_dict()
-                ec.selected_hubs = select_hubs()
-                data_per_client_dict = create_data()
-                clients = create_clients(data_per_client_dict)
+        for topo in topologies:
+            ec.topology_technique =topo
+            data_to_pickle[ec.data_set.name][ec.topology_technique] = {}
 
-                if ec.environment == Env.Distributed:
-                    all_init(clients)
-                    extract_info(clients,0)
+            for client_size  in client_sizes:
+                ec.num_clients = client_size
+                data_to_pickle[ec.data_set.name][ec.topology_technique][ec.num_clients] = {}
 
-                    for t in range(1, ec.iterations):
-                        print("*********** iteration:",t,"***********")
-                        all_send(clients)
-                        all_receive(clients)
-                        all_compute(clients, t)
-                        extract_info(clients,t)
+                for client_hub_net in client_hub_nets:
+                    ec.client_hub_net = client_hub_net
+                    data_to_pickle[ec.data_set.name][ec.topology_technique][ec.num_clients][ec.client_hub_net] = {}
+
+
+
+                    for num_run in range(ec.num_runs):
+                        #data_to_pickle[ec.algorithm][ec.client_hub_net][num_run] = {}
+
+                        ec.num_run = num_run
+                        ec.neighbors_dict = get_neighbors_dict()
+                        ec.selected_hubs = select_hubs()
+                        data_per_client_dict = create_data()
+                        clients = create_clients(data_per_client_dict)
+
+                        if ec.environment == Env.Distributed:
+                            all_init(clients)
+                            extract_info(clients,0)
+
+                            for t in range(1, ec.iterations):
+                                print("*********** iteration:",t,"***********")
+                                all_send(clients)
+                                all_receive(clients)
+                                all_compute(clients, t)
+                                extract_info(clients,t)
 
 
 
